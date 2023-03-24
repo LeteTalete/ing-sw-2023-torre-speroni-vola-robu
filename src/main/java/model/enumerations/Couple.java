8b4ffaebdb9c68f538dfa2100249f6333ff044df
letyn <letiziamaria.torre@mail.polimi.it>
2023-03-24 15:58:58 +0100

    adding couple class

---
+++ src/main/java/model/enumerations/Couple.java
@@ -0,0 +1,17 @@
+package model.enumerations;
+
+public class Couple {
+    private Tile tileType;
+
+    private State state;
+
+    public void getTile() {
+
+    }
+
+    public void setTile() {}
+
+    public void getState() {}
+
+    public void setState() {}
+}
