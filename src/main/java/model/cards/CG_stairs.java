949fb501de602e51752356baa00cd365ad255f12
Riccardo-Speroni <riccardo.speroni@mail.polimi.it>
2023-03-24 17:37:42 +0100

    Implemented Deck class and fixed classes

--- src/main/java/model/cards/CG_stairs.java
+++
@@ -1,8 +0,0 @@
-package model.cards;
-
-public class CG_stairs extends CommonGoalCard{
-    public boolean checkConditions(){
-        boolean returnValue;
-        return returnValue;
-    }
-}
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,5 @@
 package org.example;
 
-import model.board.Space;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 
 
