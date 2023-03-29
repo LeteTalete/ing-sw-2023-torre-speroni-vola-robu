2451f223b0839ededd916de51110190055bc92c0
Davide <davide.vola@mail.polimi.it>
2023-03-29 16:23:35 +0200

    added json file

--- src/main/java/model/cards/PersonalGoalCard.java
+++ src/main/java/model/cards/PersonalGoalCard.java
@@ -1,15 +1,52 @@
 package model.cards;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PersonalGoalCard extends GoalCard {
-    private ArrayList<Space> positions;
+    private ArrayList<String> positionCards;
+    public void setPositionCards(int numPersonalCard){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            File jsonFile = new File("src/main/java/JSONFile/PersonalGoals.json");
+            JsonNode rootNode = objectMapper.readTree(jsonFile);
+            JsonNode cardNode = null;
+            switch (numPersonalCard){
+                case 1:
+                    cardNode = rootNode.get("PersonalGoals1");
+                    break;
+                case 2:
+                    cardNode = rootNode.get("PersonalGoals2");
+                    break;
+                case 3:
 
-    public boolean checkConditions(){
-        boolean returnValue;
+            }
+            for (JsonNode node : cardNode) {
+                this.positionCards.add(node.fields().next().getValue().asText());
+            }
+
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
         return returnValue;
     }
 
-    public void getPositions(){
+    public int checkConditions(JsonNode position){
+
+
+
     }
+
+
+
+
 }
