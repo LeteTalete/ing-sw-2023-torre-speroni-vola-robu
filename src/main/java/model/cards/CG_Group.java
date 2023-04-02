fef97115601ed47fc017c19a5a61ab65b18c49ff
vlad-robu <vladalexandru.robu@mail.polimi.it>
2023-04-02 18:19:53 +0200

    Added constructor for CG_Group and Position

--- src/main/java/model/cards/CG_Group.java
+++ src/main/java/model/cards/CG_Group.java
@@ -1,4 +1,23 @@
 package model.cards;
 
-public class CG_Group {
+import model.Position;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CG_Group extends CommonGoalCard {
+    private int ID;
+    public CG_Group(int id) {
+        this.ID = id;
+    }
+
+    public int getID() {
+        return this.ID;
+    }
 }
