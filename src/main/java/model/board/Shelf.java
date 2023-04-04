ec8583a313957786feb39b1a4ca0ac91ef3747f2
vlad-robu <vladalexandru.robu@mail.polimi.it>
2023-04-04 16:07:52 +0200

    Shelf - fixed/added getters

--- src/main/java/model/board/Shelf.java
+++ src/main/java/model/board/Shelf.java
@@ -6,8 +6,9 @@ import model.enumerations.State;
 public class Shelf {
     public static final int ROWS = 6;
     public static final int COLUMNS = 5;
-    private Couple shelfsMatrix[][];
+    private Couple[][] shelfsMatrix = new Couple[ROWS][COLUMNS];
     private int freeSlots[];
+    private int countOccurrences[] = new int[12]; // Questo deve andare dentro la couple
 
     //getMaxFree takes as an argument the number of column in which we want to count the empty spaces
     //if numberColumn > 4 it means we're about to check all the columns and find the max of empty
@@ -42,20 +43,31 @@ public class Shelf {
         return false;
     }
 
-    public void setFreeSlots(int column, int row)
+    public void setFreeSlots(int row, int column)
     {
-        this.shelfsMatrix[column][row].setState(State.EMPTY);
+        this.shelfsMatrix[row][column].setState(State.EMPTY);
     }
 
-    public void setCoordinate(int column, int row, Couple chosen)
+    public void setCoordinate(int row, int column, Couple chosen)
     {
-        this.shelfsMatrix[column][row].setState(chosen.getState());
-        this.shelfsMatrix[column][row].setTile(chosen.getTile());
+        this.shelfsMatrix[row][column].setState(chosen.getState());
+        this.shelfsMatrix[row][column].setTile(chosen.getTile());
     }
 
     public Couple getCoordinate(int column, int row)
     {
         return this.shelfsMatrix[column][row];
+    }
+
+    public Couple[][] getShelfsMatrix() {
+        return this.shelfsMatrix;
+    }
+
+    public int[] getCountOccurrences() {
+        return this.countOccurrences;
+    }
+
+    public Shelf(){
     }
 
 }
