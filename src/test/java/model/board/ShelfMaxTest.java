93cb7466c92dbd1ad681f04565a2334b6cd7b3a1
letyn <letiziamaria.torre@mail.polimi.it>
2023-04-04 14:06:58 +0200

    completed tests for getMaxFree and checkShelfFull

--- src/test/java/model/board/ShelfMaxTest.java
+++ src/test/java/model/board/ShelfMaxTest.java
@@ -1,18 +1,41 @@
 package model.board;
 
 import model.enumerations.Couple;
+import model.enumerations.State;
+import model.enumerations.Tile;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShelfMaxTest {
 
+    private Shelf s;
+
+    private Couple[][] shelfsMatrix;
+    private Couple dummy;
+
     @Test
-    void getMaxFree() {
-        Couple dummy = new Couple()
+    void testMaxFree() {
+        dummy.setState(State.PICKED);
+        for(int i=0; i<5; i++)
+        {
+            this.shelfsMatrix[i][0]=dummy;
+        }
+        assertEquals(4,s.getMaxFree(5));
     }
 
     @Test
-    void checkShelfFull() {
+    void testShelfFull() {
+        dummy.setState(State.PICKED);
+        for(int i=0; i<5; i++)
+        {
+            this.shelfsMatrix[i][0]=dummy;
+            this.shelfsMatrix[i][1]=dummy;
+            this.shelfsMatrix[i][2]=dummy;
+            this.shelfsMatrix[i][3]=dummy;
+            this.shelfsMatrix[i][4]=dummy;
+
+        }
+        assertEquals(0,s.getMaxFree(5));
     }
 }
