a4ce8df013dc7e11642ad76020d1cde0dd1c9be4
letyn <letiziamaria.torre@mail.polimi.it>
2023-04-04 13:42:58 +0200

    updated Shelf with setter and getter methods to fill up a player's shelf; built a tester for getMaxFree and checkShelfFulll methods

---
+++ src/test/java/model/board/ShelfMaxTest.java
@@ -0,0 +1,18 @@
+package model.board;
+
+import model.enumerations.Couple;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShelfMaxTest {
+
+    @Test
+    void getMaxFree() {
+        Couple dummy = new Couple()
+    }
+
+    @Test
+    void checkShelfFull() {
+    }
+}
