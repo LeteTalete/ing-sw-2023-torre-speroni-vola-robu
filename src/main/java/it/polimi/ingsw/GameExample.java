496ab145a92f459a98d6abbd1dc7c72c733591b5
vlad-robu <vladalexandru.robu@mail.polimi.it>
2023-04-29 15:22:52 +0200

    minor fixes + score calculation

--- src/main/java/it/polimi/ingsw/GameExample.java
+++ src/main/java/it/polimi/ingsw/GameExample.java
@@ -3,8 +3,10 @@ package it.polimi.ingsw;
 import it.polimi.ingsw.controller.GameController;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.Position;
+import it.polimi.ingsw.model.cards.CommonGoalCard;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GameExample {
@@ -26,59 +28,57 @@ public class GameExample {
 
         GameController game = new GameController(players, 0);
         game.generateCGC(players.size());
-        game.generatePGC(players);
+        game.generatePGC(game.getPlayers());
         game.chooseFirstPlayer();
         System.out.println("The first player will be: " + game.getCurrentPlayer().getNickname());
+        System.out.println();
 
-        int undo = 0;
         int end = 0;
         int endTurn = 0;
         while( end != players.size() - 1 ){
             System.out.println("The current player is: " + game.getCurrentPlayer().getNickname());
             System.out.println();
             endTurn = 0;
-            undo = 0;
 
-            while( endTurn != 1 ){
+            while( endTurn != 1 ) {
                 ArrayList<Position> hand = new ArrayList<>();
                 game.getGameBoard().printBoard();
                 System.out.println();
                 hand = game.getCurrentPlayer().chooseTiles(game.getGameBoard());
                 System.out.print("Tiles chosen: ");
-                for ( Position position : hand ){
+                for (Position position : hand) {
                     System.out.print(game.getGameBoard().getCouple(position).getTile().getTileType() + " ");
                 }
                 System.out.println();
                 System.out.println("To start over write 'undo' else to continue write anything else ");
-                if ( sc.nextLine().equals("undo") ) {
-                    undo = 1;
-                }
-                    while (undo == 0 && endTurn != 1) {
+                if ( !sc.nextLine().equals("undo") ) {
+                    while (endTurn != 1) {
                         hand = game.getCurrentPlayer().choseOrder(hand, game.getGameBoard());
                         System.out.print("Order chosen: ");
                         int k = 1;
-                        for ( Position position : hand ){
+                        for (Position position : hand) {
                             System.out.print( k + ". " + game.getGameBoard().getCouple(position).getTile().getTileType() + " ");
                             k++;
                         }
                         System.out.println();
                         System.out.println("To go back and choose different tiles write 'undo' else to continue write anything else ");
-                        if ( sc.nextLine().equals("undo") ) {
-                            undo = 1;
+                        if (sc.nextLine().equals("undo")) {
+                            break;
                         }
-                        while ( undo == 0 && endTurn != 1) {
+                        while (endTurn != 1) {
                             game.getCurrentPlayer().getMyShelf().printShelf();
                             System.out.println("To go back and choose a different tiles order write 'undo' else to continue write anything else ");
-                            if ( !sc.nextLine().equals("undo") ) {
+                            if (sc.nextLine().equals("undo")) {
+                                break;
+                            } else {
                                 game.getCurrentPlayer().chooseColumn(hand, game.getGameBoard());
                                 game.getCurrentPlayer().getMyShelf().printShelf();
                                 endTurn = 1;
-                            } else {
-                                break;
                             }
                         }
                     }
-                undo = 0;
+                }
+
             }
 
             if ( game.getCurrentPlayer().getMyShelf().checkShelfFull() ){
@@ -87,12 +87,28 @@ public class GameExample {
                 end++;
             }
 
+            for (CommonGoalCard card : game.getCommonGoalCards() ) {
+                if ( card.checkConditions(game.getCurrentPlayer().getMyShelf()) == 1 ) {
+                    game.getCurrentPlayer().setScore(card.getPoints().pop());
+                }
+            }
+
             game.nextTurn();
             System.out.println();
             System.out.println();
-
         }
 
+        System.out.println("Calculating scores...");
+        game.calculateScore();
+        Player highestScore = game.getPlayers().get(0);
+        for ( Player player : game.getPlayers() ){
+            System.out.println( player.getNickname() + "'s score is: " + player.getScore());
+            if (player.getScore() > highestScore.getScore()) {
+                highestScore = player;
+            }
+        }
+        System.out.println();
+        System.out.println("The winner is: " + highestScore.getNickname());
     }
 
 }
