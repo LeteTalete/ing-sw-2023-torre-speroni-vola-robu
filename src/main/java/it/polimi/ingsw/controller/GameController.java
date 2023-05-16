38855bd356bc903d933da1ffcc0edaa440ba2c11
vlad-robu <vladalexandru.robu@mail.polimi.it>
2023-05-16 18:57:06 +0200

    added chooseTiles to GameController

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -1,14 +1,18 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.Exceptions.InvalidChoiceFormatException;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.Position;
 import it.polimi.ingsw.model.board.LivingRoom;
 import it.polimi.ingsw.model.cards.CommonGoalCard;
 import it.polimi.ingsw.server.ServerManager;
+import it.polimi.ingsw.server.StaticStrings;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class GameController {
     private Game model;
@@ -23,6 +27,7 @@ public class GameController {
         model.createGameBoard(playersList.size());
         master = serverMaster;
     }
+
     public GameController(ArrayList<Player> playersList, String id) {
         model = new Game(id, this);
         model.setPlayers(playersList);
@@ -30,6 +35,7 @@ public class GameController {
         gameId = id;
         model.createGameBoard(playersList.size());
     }
+
     public void initialize() throws RemoteException {
         model.initialize();
     }
@@ -38,9 +44,30 @@ public class GameController {
         master.notifySinglePlayer(name, message);
     }
 
-    public void notifyAllPlayers(String message){
+    public void notifyAllPlayers(String message) {
         master.notifyAllPlayers(gameId, message);
     }
+
+    public void chooseTiles(String username, String userInput) throws RemoteException {
+
+        ArrayList<Position> choice = new ArrayList<>();
+
+        for (int i = 0; i < userInput.length(); i++) {
+            if (i % 3 == 0) {
+                choice.add(new Position());
+                choice.get(i / 3).setX(userInput.charAt(i) - 48);
+            } else if (( i + 1 ) % 3 != 0) {
+                choice.get(( i - 1 ) / 3).setY(userInput.charAt(i) - 48);
+            }
+        }
+
+        if (this.getGameBoard().checkPlayerChoice(choice)) {
+            master.notifySinglePlayer(username, StaticStrings.OK);
+        }
+        // needs else with StaticString.NO in case the choice is wrong
+
+    }
+
 
     public void generateCGC(){
         model.generateCGC(model.getPlayers().size());
