0c5743a1a354bb2df8e56988ff140465774da175
letyn <letiziamaria.torre@mail.polimi.it>
2023-05-26 12:19:17 +0200

    implemented logger on serverExecutable (needs to be implemented eveywhere for better handling of our code)

--- src/main/java/it/polimi/ingsw/ServerExecutable.java
+++ src/main/java/it/polimi/ingsw/ServerExecutable.java
@@ -6,9 +6,17 @@ import it.polimi.ingsw.server.ServerManager;
 import java.io.IOException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ServerExecutable {
+
+    private static Logger fileLog = LogManager.getRootLogger();
     public static void main(String[] args) {
+        fileLog.debug("Server started");
+        fileLog.info("Server started");
+        fileLog.warn("Server started");
+        fileLog.error("Server started");
         try {
             ServerManager serverManager = new ServerManager();
             Registry registry = LocateRegistry.createRegistry(8089);
