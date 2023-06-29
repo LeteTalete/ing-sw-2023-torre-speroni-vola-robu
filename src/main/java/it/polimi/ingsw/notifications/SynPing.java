6aa0b70c890aa2ab10b5fa6e6afea9219cf8392b
Riccardo-Speroni <riccardo.speroni@mail.polimi.it>
2023-06-29 17:23:24 +0200

    added javadoc comments

--- src/main/java/it/polimi/ingsw/notifications/SynPing.java
+++ src/main/java/it/polimi/ingsw/notifications/SynPing.java
@@ -8,6 +8,12 @@ import java.rmi.RemoteException;
 /**Ping message sent to the client periodically*/
 
 public class SynPing implements Response {
+
+    /**
+     * Method handleResponse is used to handle a response sent by server socket
+     * @param responseHandler - the response handler
+     * @throws RemoteException
+     */
     @Override
     public void handleResponse(ResponseHandler responseHandler) throws RemoteException {
         responseHandler.handle(this);
